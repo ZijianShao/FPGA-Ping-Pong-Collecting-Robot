cb03371899db35dfba3d9a93b5d121a6846fcd82
ZijianShao <97234140+ZijianShao@users.noreply.github.com>
2023-03-19 00:42:23 +0800

    Add files via upload

---
+++ 0Final_Robei_Jichuang/verilog/filelist.v
@@ -0,0 +1,16 @@
+C:/Users/Lenovo/Desktop/jcs/q13/0Final_Robei_Jichuang/verilog/HighLevel.v
+C:/Users/Lenovo/Desktop/jcs/q13/0Final_Robei_Jichuang/verilog/MUX.v
+C:/Users/Lenovo/Desktop/jcs/q13/0Final_Robei_Jichuang/verilog/Move_CTR.v
+C:/Users/Lenovo/Desktop/jcs/q13/0Final_Robei_Jichuang/verilog/PWM.v
+C:/Users/Lenovo/Desktop/jcs/q13/0Final_Robei_Jichuang/verilog/RB_Final.v
+C:/Users/Lenovo/Desktop/jcs/q13/0Final_Robei_Jichuang/verilog/aviodLogic.v
+C:/Users/Lenovo/Desktop/jcs/q13/0Final_Robei_Jichuang/verilog/duty_decoder.v
+C:/Users/Lenovo/Desktop/jcs/q13/0Final_Robei_Jichuang/verilog/iic_send.v
+C:/Users/Lenovo/Desktop/jcs/q13/0Final_Robei_Jichuang/verilog/play_init.v
+C:/Users/Lenovo/Desktop/jcs/q13/0Final_Robei_Jichuang/verilog/play_qianjin.v
+C:/Users/Lenovo/Desktop/jcs/q13/0Final_Robei_Jichuang/verilog/play_tingzhi.v
+C:/Users/Lenovo/Desktop/jcs/q13/0Final_Robei_Jichuang/verilog/play_youzhuan.v
+C:/Users/Lenovo/Desktop/jcs/q13/0Final_Robei_Jichuang/verilog/play_zuozhuan.v
+C:/Users/Lenovo/Desktop/jcs/q13/0Final_Robei_Jichuang/verilog/voice_play_control.v
+C:/Users/Lenovo/Desktop/jcs/q13/0Final_Robei_Jichuang/verilog/voice_play_cptBlock.v
+
